b817fe2e5855adf643594233a8f398a8aa65b297
zenklot <zenklot11@gmail.com>
2022-11-06 16:00:51 +0700

    :sparkles: : add Router, Navigation

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,25 @@
+import { Outlet, Link } from "react-router-dom";
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
+
+import './navigation.styles.scss'
+
+const Navigation = () => {
+
+    return (
+        <>
+            <div className="navigation">
+                <Link to='/' className="logo-container">
+                    <CrwnLogo />
+                </Link>
+                <div className="nav-links-container">
+                <Link to='/shop' className="nav-link">
+                    SHOP
+                </Link>
+                </div>
+            </div>
+            <Outlet />
+        </>
+    )
+}
+
+export default Navigation;
