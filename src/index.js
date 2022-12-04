5b890cf3c4c415a52227526cff274099f5312bde
zenklot <zenklot11@gmail.com>
2022-12-04 07:27:40 +0700

    setup redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 import { UserProvider } from "./contexts/user.context";
 import { CategoriesProvider } from "./contexts/categories.context";
 import { CartProvider } from "./contexts/cart.context";
@@ -11,15 +13,17 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <UserProvider>
+          <CategoriesProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </CategoriesProvider>
+        </UserProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,12 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 
+import { rootReducer } from "./root-reducer";
+
+// Middleware
+const middleWares = [logger];
+
+const composedEnhancers = compose(applyMiddleware(...middleWares));
+
 // root-reducer
+export const store = createStore(rootReducer, undefined, composedEnhancers);
