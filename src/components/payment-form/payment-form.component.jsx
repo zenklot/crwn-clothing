72b88b1c7f4545b94a54f3e759b227d0b45cf2ac
zenklot <zenklot11@gmail.com>
2022-12-24 16:48:55 +0700

    Implement Stripe

---
+++ src/components/payment-form/payment-form.component.jsx
@@ -0,0 +1,72 @@
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import { useSelector } from "react-redux";
+import { useState } from "react";
+import { selectCurrentUser } from "../../store/user/user.selector";
+import { selectCartTotal } from "../../store/cart/cart.selector";
+
+import { BUTTON_TYPE_CLASS } from "../button/button.component";
+import {
+  PaymentFormContainer,
+  FormContainer,
+  PaymentButton,
+} from "./payment-form.styles";
+
+const PaymentForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const amount = useSelector(selectCartTotal);
+  const currentUser = useSelector(selectCurrentUser);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const paymentHandler = async (e) => {
+    e.preventDefault();
+    if (!stripe || !elements) return;
+    setIsLoading(true);
+    const response = await fetch("/.netlify/functions/create-payment-intent", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ amount: amount * 100 }),
+    }).then((res) => res.json());
+
+    const {
+      paymentIntent: { client_secret },
+    } = response;
+
+    const paymentResult = await stripe.confirmCardPayment(client_secret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: currentUser ? currentUser.displayName : "Guest",
+        },
+      },
+    });
+
+    if (paymentResult.error) {
+      alert(paymentResult.error);
+    } else {
+      if (paymentResult.paymentIntent.status === "succeeded") {
+        alert("Payment Success");
+      }
+    }
+
+    setIsLoading(false);
+  };
+
+  return (
+    <PaymentFormContainer>
+      <FormContainer onSubmit={paymentHandler}>
+        <CardElement />
+        <PaymentButton
+          isLoading={isLoading}
+          buttonType={BUTTON_TYPE_CLASS.inverted}
+        >
+          Pay Now
+        </PaymentButton>
+      </FormContainer>
+    </PaymentFormContainer>
+  );
+};
+
+export default PaymentForm;
